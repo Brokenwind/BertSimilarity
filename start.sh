9d2fb8176075bc76c57070a0f25b574158f95d0f
Brokenwind <wangkun6536@163.com>
2020-05-10 18:25:01 +0800

    Change: update the start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-read -p "running mode: train/eval/predict): " mode
+read -p "running mode: train/eval/infer): " mode
 
 # 加入环境变量
 CUR_PATH=`pwd`
 export PYTHONPATH=$CUR_PATH:$PYTHONPATH
-echo "starting "$mode" process"
 
-python similarity.py --mode=$mode > "similarity_"$mode.log 2>&1 &
+if [ "$mode" == "train" ];then
+  python similarity.py --mode=$mode > logs/"similarity_"$mode.log 2>&1 &
+  echo "check train log with command: tail -f logs/similarity_$mode.log"
+elif [ "$mode" == "eval" ];then
+  python similarity.py --mode=$mode > logs/"similarity_"$mode.log 2>&1 &
+  echo "check eval log with command: tail -f logs/similarity_$mode.log"
+elif [ "$mode" == "infer" ];then
+  python similarity.py --mode=$mode
+else
+  echo "parameter error, choose from train/eval/infer"
+  exit 1
+fi
